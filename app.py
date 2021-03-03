d7f1467059a55bba8d5d2e9f493115a5efcd8f10
Denis Ahinski <ahinski.denis@gmail.com>
2021-03-03 22:05:50 +0300

    feat: update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+from static.models.predict import App
 
 app = Flask(__name__)
 
@@ -9,7 +10,12 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    pass
+    text = [request.form['tweet_text']][0]
+    predict = App(text)
+    probability = predict.calculate()
+    print('here is is')
+    print(probability)
+    return render_template('index.html', prediction='Probability of tweet being about real disaster is {}'.format(probability))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/models/predict.py
+++ static/models/predict.py
@@ -10,27 +10,25 @@ class App():
     ''' Class for API application 'Probability of Disaster Tweet'
 
     Attributes:
-    tweet - tweet in json form {tweet: str(text)}
+    tweet - text from tweet
     
     Methods:
     calculate() returns probability in json form {probability: result}
     '''
     def __init__(self, tweet):
-        self.text = Text(tweet.tweet)
+        self.text = Text(tweet)
 
     def calculate(self):
         'Calculates the probability of tweet being real about disaster and return JSON'
-        clf_LogReg = pickle.load(open('LogReg_model.sav', 'rb'))
-        clf_RF = pickle.load(open('RF_model.sav', 'rb'))
-        clf_NB = pickle.load(open('NB_model.sav', 'rb'))
+        clf_LogReg = pickle.load(open('static/models/LogReg_model.sav', 'rb'))
+        clf_RF = pickle.load(open('static/models/RF_model.sav', 'rb'))
+        clf_NB = pickle.load(open('static/models/NB_model.sav', 'rb'))
         text = self.text.preprocess()
         predict_LogReg = clf_LogReg.predict_proba(text)[:, 1]
         predict_RF= clf_RF.predict_proba(text)[:, 1]
         predict_NB = clf_NB.predict_proba(text)[:, 1]
         result = math.floor(((predict_LogReg + predict_RF + predict_NB) / 3)[0] * 100) / 100
-        return jsonify(
-                    {"probability": str(result)}
-                ),
+        return result
 
 class Text():
     ''' Class for representing text
@@ -57,20 +55,25 @@ class Text():
         if (self.__preprocessed == False):
             # lowercase
             self.text = self.text.lower()
+
             # removing links, mentions, and hashtag (only the symbol)
             self.text = re.sub(r'https?://\S+|www\.\S+','', self.text)
             self.text = re.sub(r'@[A-Za-z0-9]+','', self.text)
             self.text = re.sub(r'#','', self.text)
+
             # removing punctuation
             self.text = re.sub(r'[^\w\s]','', self.text)
+
             # lemmatizing text
             self.text = self.__lemmatize()
+
             # removing stopwords
             all_stopwords = set(nltk.corpus.stopwords.words('english'))
             self.text = ' '.join([word for word in self.text.split() if word not in (all_stopwords)])
+
             # removing words with numbers and letters along
             self.text = ' '.join(w for w in self.text.split() if not any(j.isdigit() for j in w))
-            vect = pickle.load(open('vectorizer.sav', 'rb'))
+            vect = pickle.load(open('static/models/vectorizer.sav', 'rb'))
             self.text = vect.transform([self.text])
             self.text = self.text.todense()
             self.__preprocessed = True
